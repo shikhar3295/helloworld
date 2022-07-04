baabaef4da17c7c51760e66bf358d1890c99afcf
Srujan Sharma <shikhar3295@gmail.com>
2022-07-04 12:34:34 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World. It is a pleasure to be alive! 
+          Hello World. It's a pleasure to be alive! 
         </p>
         <a
           className="App-link"
